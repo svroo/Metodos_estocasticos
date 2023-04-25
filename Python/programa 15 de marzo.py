052ca2e8446a423e83bfadfa933c6fd9e00b7438
Rodrigo <rod_explosionatomica@hotmail.com>
2023-04-25 11:43:20 -0600

    update

--- Python/programa 15 de marzo.py
+++ Python/programa 15 de marzo.py
@@ -1,19 +1,20 @@
 # Programa que va a calcular el estado del clima usando una cadena de markov
-# 
+#
 
 
 # Librerias
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 
 # Inicializamos la semilla
 np.random.seed(3)
 
 # almacenamos los dos estados posibles
-statesData = ['Sunny', 'Rainny']
+statesData = ["Sunny", "Rainny"]
 
 # Almacenamos los estados de transicion
-transitionStates = [['SuSu', 'SuRa'], ['RaRa', 'RaSu']]
+transitionStates = [["SuSu", "SuRa"], ["RaRa", "RaSu"]]
 
 # Matriz de probabilidades
 transitionMatrix = [[0.80, 0.20], [0.25, 0.75]]
@@ -27,45 +28,48 @@ numDays = 365
 # Inicializamos una variable
 todayPrediction = statesData[0]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for i in range(1, numDays):
-        
-        if todayPrediction == 'Sunny':
-            Transcondition = np.random.choice(transitionStates[0], replace=True, p=transitionMatrix[0])
-            if Transcondition == 'Susu':
+        if todayPrediction == "Sunny":
+            Transcondition = np.random.choice(
+                transitionStates[0], replace=True, p=transitionMatrix[0]
+            )
+            if Transcondition == "Susu":
                 pass
             else:
-                todayPrediction = 'Rainny'
-        
-        elif todayPrediction == 'Rainny':
-            Transcondition = np.random.choice(transitionStates[1], replace=True, p=transitionMatrix[1])
-            if Transcondition =='RaRa':
+                todayPrediction = "Rainny"
+
+        elif todayPrediction == "Rainny":
+            Transcondition = np.random.choice(
+                transitionStates[1], replace=True, p=transitionMatrix[1]
+            )
+            if Transcondition == "RaRa":
                 pass
             else:
-                todayPrediction = 'Sunny'
-                
+                todayPrediction = "Sunny"
+
         weatheForecasting.append(todayPrediction)
-    
+
     # Mostramos los resultados que se almacenaron en la lista
-    print(weatheForecasting)
-    
+    print(Counter(weatheForecasting))
+
     # Graficamos
-    fig, (ax, ax1) = plt.subplots(ncols=2) 
-    fig.suptitle('Graficas')
-     
-    # plot sensillo  
+    fig, (ax, ax1) = plt.subplots(ncols=2)
+    fig.suptitle("Graficas")
+
+    # plot sensillo
     ax.plot(weatheForecasting)
-    ax.set_title('Grafico de weatherForecastin')
-    ax.set_xlabel('Conteo')
-    ax.set_ylabel('Estados posibles')
-    
+    ax.set_title("Grafico de weatherForecastin")
+    ax.set_xlabel("Conteo")
+    ax.set_ylabel("Estados posibles")
+
     # Histograma
     ax1.hist(weatheForecasting)
-    ax1.set_title('Histograma de weatherForecastin')
-    ax1.set_ylabel('Conteo')
-    ax1.set_xlabel('Estados posibles')
-    
+    ax1.set_title("Histograma de weatherForecastin")
+    ax1.set_ylabel("Conteo")
+    ax1.set_xlabel("Estados posibles")
+
     # Ajustamos el tamaño
     fig.set_size_inches(15.0, 7.0)
-    
+
     plt.show()
--- Python/tercero.py
+++ Python/tercero.py
@@ -8,27 +8,27 @@ from numpy import pi, sin, random
 
 
 #### codigo #####
-def rand_array(n)-> np.array:
-
-    lista =[0] * n
+def rand_array(n) -> np.array:
+    lista = [0] * n
 
     for i in range(n):
         lista[i] = random.random()
-    
+
     return np.array(lista)
 
-def bufon (throws):
 
+def bufon(throws):
     x = rand_array(throws)
     theta = rand_array(throws)
-    
+
     # los convertimos a angulos
-    theta = 0.5 * pi *theta
+    theta = 0.5 * pi * theta
 
     # creamos los ensayos del lanzamiento
-    hits = x <= 0.5*sin(theta)
+    hits = x <= 0.5 * sin(theta)
 
     return throws / sum(hits)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(bufon(1000000))
